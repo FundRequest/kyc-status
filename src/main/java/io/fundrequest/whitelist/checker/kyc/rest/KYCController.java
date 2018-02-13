9944e929a569ee5514c8495c1bb288a8a3b8366d
Quinten De Swaef <quintendeswaef@gmail.com>
2018-02-13 16:08:40 +0100

    pending instead of empty

--- src/main/java/io/fundrequest/whitelist/checker/kyc/rest/KYCController.java
+++ src/main/java/io/fundrequest/whitelist/checker/kyc/rest/KYCController.java
@@ -1,6 +1,8 @@
 package io.fundrequest.whitelist.checker.kyc.rest;
 
 import io.fundrequest.whitelist.checker.kyc.dto.KYCResultDto;
+import io.fundrequest.whitelist.checker.kyc.dto.KYCStatusDto;
+import io.fundrequest.whitelist.checker.kyc.dto.KYCStatusEnum;
 import io.fundrequest.whitelist.checker.kyc.dto.ProgressDto;
 import io.fundrequest.whitelist.checker.kyc.service.KYCService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +29,11 @@ public class KYCController {
     @CrossOrigin(origins = "*")
     public KYCResultDto search(@PathVariable("address") final String address) {
         return kycService.search(address)
-                .orElse(null);
+                .orElse(new KYCResultDto()
+                        .setAddress(address)
+                        .setMessage("")
+                        .setReferralCount(0L)
+                        .setStatus(KYCStatusEnum.PENDING.toDto())
+                );
     }
 }
