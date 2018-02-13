59a300fceb39cd646dd17b85ba8ef4d18bbf6e69
Quinten De Swaef <quintendeswaef@gmail.com>
2018-02-13 16:08:50 +0100

    clenaup

--- src/main/java/io/fundrequest/whitelist/checker/kyc/rest/KYCController.java
+++ src/main/java/io/fundrequest/whitelist/checker/kyc/rest/KYCController.java
@@ -1,16 +1,11 @@
 package io.fundrequest.whitelist.checker.kyc.rest;
 
 import io.fundrequest.whitelist.checker.kyc.dto.KYCResultDto;
-import io.fundrequest.whitelist.checker.kyc.dto.KYCStatusDto;
 import io.fundrequest.whitelist.checker.kyc.dto.KYCStatusEnum;
 import io.fundrequest.whitelist.checker.kyc.dto.ProgressDto;
 import io.fundrequest.whitelist.checker.kyc.service.KYCService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/kyc")
